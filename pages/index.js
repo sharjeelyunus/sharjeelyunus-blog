0199b640f1c5849febb86ca2f90f9a89f7772d48
sharjeelyunus <sharjeel924@gmail.com>
2022-03-14 22:07:33 +0500

    Setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Sharjeel Yunus | Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  )
+}
