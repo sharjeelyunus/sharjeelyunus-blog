c4bfe2f4af612501a0eca4b135458e7d17880e93
sharjeelyunus <sharjeel924@gmail.com>
2022-03-17 02:55:07 +0500

    Graphql Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: "NextJS Testing", excerpt: 'Learn NextJS testing' },
-  { title: "NextJS with tailwaind", excerpt: 'Learn NextJS with tailwaind' },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -26,4 +22,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
---
+++ services/index.js
@@ -0,0 +1,39 @@
+import { request, gql } from 'graphql-request';
+
+const qraphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              auther {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const result = await request(qraphqlAPI, query);
+
+    return result.postsConnection.edges;
+}
